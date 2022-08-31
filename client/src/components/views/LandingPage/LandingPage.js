055833130a9144121479069f7b9d8175c6bc71ff
braverokmc79 <braverokmc79@gmail.com>
2022-09-01 08:54:02 +0900

    21.데이터 Flow & Axios

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Axios from 'axios';
 
 function LandingPage() {
+
+  useEffect(() => {
+    Axios.get("http://localhost:5000/api/hello")
+      .then(res => {
+        console.log(res.data);
+      });
+  }, []);
+
+
   return (
     <div>LandingPage 랜딩페이지</div>
   )
