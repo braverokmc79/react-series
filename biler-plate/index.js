c95303fc3d1ec2f090565982e009b27b330de19d
braverokmc79 <braverokmc79@gmail.com>
2022-08-31 12:00:57 +0900

    9.비밀 설정 정보 관리

--- biler-plate/index.js
+++ biler-plate/index.js
@@ -1,26 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const mongodbURI = require('./mongodbURI')
 const mongoose = require("mongoose");
 const { User } = require("./models/User");
 const bodyParser = require("body-parser");
+const config = require("./config/key");
 
+//const mongodbURI = require('./mongodbURI')
 
 //aplication/json
 app.use(bodyParser.json());
 //application/x-www-form-unlencoded
 // body-parser deprecated undefined extended
 //app.use(bodyParser.urlencoded({ extends: true }));
-
-
 //Mongoose 6은 항상 useNewUrlParser, useUnifiedTopology 및 useCreateIndex가 true이고 useFindAndModify가 false인 것처럼 작동
-mongoose.connect(mongodbURI,
+
+
+
+mongoose.connect(config.mongoURI,
 ).then(() => console.log("MongoDB Connected...")).catch(err => console.error("에러 :", err));
 
+console.log("ENV 정보 : ", process.env.NODE_ENV);
+
+
+
 app.get('/', (req, res) => { res.send('Hello World! 안녕하세요.11') })
-
-
 app.post('/register', (req, res) => {
     //회원 가입 할때 필요한 정보들을  client 에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
