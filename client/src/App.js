07ffda3231842aecf7b86bde38d658e0321e478c
braverokmc79 <braverokmc79@gmail.com>
2022-09-01 11:24:31 +0900

    25.Antd CSS Framwork 26.Redux 기초

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/views/LandingPage/LandingPage';
 import LoginPage from './components/views/LoginPage/LoginPage';
 import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
